d94d89cada002c1f5724cad17c9ff70a14d1af13
EgorF56 <egorfuture56@gmail.com>
2026-04-30 12:32:15 +0300

    Creating logic of domain

--- app/src/main/java/com/example/shoppinglist/domain/EditShopItemUseCase.kt
+++ app/src/main/java/com/example/shoppinglist/domain/EditShopItemUseCase.kt
@@ -1,7 +1,7 @@
 package com.example.shoppinglist.domain
 
-class EditShopItemUseCase {
+class EditShopItemUseCase(private val shopListRepository: ShopListRepository) {
     fun editShopItem(item: ShopItem){
-        TODO()
+        shopListRepository.editShopItem(item)
     }
 }
--- app/src/main/java/com/example/shoppinglist/domain/ShopListRepository.kt
+++ app/src/main/java/com/example/shoppinglist/domain/ShopListRepository.kt
@@ -1,4 +1,9 @@
 package com.example.shoppinglist.domain
 
 interface ShopListRepository {
+    fun addShopItem(shopItem: ShopItem)
+    fun editShopItem(item: ShopItem)
+    fun getShopItem(shopItemId: Int): ShopItem
+    fun getShopList(): List<ShopItem>
+    fun deleteShopItem(item: ShopItem)
 }
